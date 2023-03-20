ca15c944b7db2fd8450b4b43630e677059dc4de5
Waitara <janewaitara99@gmail.com>
2023-03-20 21:16:29 +0300

    Added an empty state composable

--- app/src/main/java/com/mumbicodes/fibonacci/presentation/searchNumber/SearchScreen.kt
+++ app/src/main/java/com/mumbicodes/fibonacci/presentation/searchNumber/SearchScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mumbicodes.fibonacci.domain.model.FibonacciSearch
+import com.mumbicodes.fibonacci.presentation.components.EmptyStateComposable
 import com.mumbicodes.fibonacci.presentation.components.FibonacciCardComposable
 import com.mumbicodes.fibonacci.presentation.components.SearchFieldComposable
 import com.mumbicodes.fibonacci.presentation.util.provideShadowColor
@@ -131,53 +132,57 @@ fun SearchResults(fibonacciSearch: FibonacciSearch) {
 
 @Composable
 fun PreviousSearches(fibonacciSearches: List<FibonacciSearch>) {
-    Column {
-        Row {
-            Text(
-                text = "Recent searches",
-                style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.onSurface),
-                modifier = Modifier
-                    .weight(1f)
-            )
-
-            Spacer(modifier = Modifier.width(24.dp))
-
-            Text(
-                text = "Clear All",
-                style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary),
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .clickable(
-                        onClick = {
-                            TODO()
-                        }
-                    )
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            itemsIndexed(fibonacciSearches) { _, fibonacciSearches ->
-                FibonacciCardComposable(
+    if (fibonacciSearches.isEmpty()) {
+        EmptyStateComposable()
+    } else {
+        Column {
+            Row {
+                Text(
+                    text = "Recent searches",
+                    style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.onSurface),
                     modifier = Modifier
-                        .shadow(
-                            elevation = 16.dp,
-                            shape = MaterialTheme.shapes.small,
-                            ambientColor = provideShadowColor(),
-                            spotColor = provideShadowColor()
-                        )
-                        .fillMaxWidth()
-                        .background(
-                            color = MaterialTheme.colorScheme.surface,
-                            shape = MaterialTheme.shapes.small
-                        )
-                        .padding(12.dp),
-                    fibonacciSearch = fibonacciSearches
+                        .weight(1f)
                 )
+
+                Spacer(modifier = Modifier.width(24.dp))
+
+                Text(
+                    text = "Clear All",
+                    style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary),
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .clickable(
+                            onClick = {
+                                TODO()
+                            }
+                        )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                itemsIndexed(fibonacciSearches) { _, fibonacciSearches ->
+                    FibonacciCardComposable(
+                        modifier = Modifier
+                            .shadow(
+                                elevation = 16.dp,
+                                shape = MaterialTheme.shapes.small,
+                                ambientColor = provideShadowColor(),
+                                spotColor = provideShadowColor()
+                            )
+                            .fillMaxWidth()
+                            .background(
+                                color = MaterialTheme.colorScheme.surface,
+                                shape = MaterialTheme.shapes.small
+                            )
+                            .padding(12.dp),
+                        fibonacciSearch = fibonacciSearches
+                    )
+                }
             }
         }
     }
